1bcd11cc2c6f9a4bf528e4f599e0b330fcdfecb3
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-01-20 12:09:58 +0200

    feat: solve task 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+const path = require('path');
+fs.readdir(path.join(__dirname, '//secret-folder'), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.stat(path.join(__dirname, '//secret-folder', file), (err, stats) => {
+        if (err) {
+          console.error(err);
+        } else {
+          if (stats.isFile()) {
+            console.log(
+              `${file.split('.')[0]} - ${path.extname(
+                path.join(__dirname, '//secret-folder', file),
+              ).replace('.', '')} - ${`${parseFloat(stats.size) / 1024}kb`}`,
+            );
+          }
+        }
+      });
+    });
+  }
+});
